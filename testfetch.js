4cb377fd45553001a9f1bb7ae6cf335015bd70a0
Hafid HADDAOUI <hahaddaoui@gmail.com>
2023-07-18 18:29:53 +0200

    edit project folder

--- testfetch.js
+++ testfetch.js
@@ -1,5 +1,3 @@
-import "./styles.css";
-
 fetch("https://api-rest-todolist-4b99865c33b9.herokuapp.com/")
-  .then((res) => res.json())
-  .then((res) => console.log(res));
+  .then((response) => response.json())
+  .then((response) => console.log(response));
