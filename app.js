8fb3468802fac868a966bb33f484effa7ca9352a
Hafid HADDAOUI <hahaddaoui@gmail.com>
2023-07-19 18:32:45 +0200

    add cors middleware

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const sequelize = require('./src/db/sequelize')
 const bodyParser = require('body-parser')
-
+const cors = require('cors')
 const app = express()
 const port = process.env.PORT || 3000;
 
 app
     .use(bodyParser.json())
+    .use(cors())
 
 sequelize.InitDB();
 
